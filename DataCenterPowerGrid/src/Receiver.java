fd1925217247525b81ef3dc03ac8c672f305c5b3
Bart Wiegmans <bartwiegmans@gmail.com>
2014-09-22 13:23:06 +0200

    Add a simple Receiver class.
    Basically takes raw datagrampackets and puts them on a queue.
    We'll need to move this into a separate thread because we're
    also dealing with timeouts

---
+++ DataCenterPowerGrid/src/Receiver.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.util.concurrent.BlockingQueue;
+
+/* Receives UDP messages and puts them on a queue. Works on
+ * MulticastSockets as well (they are a subclass) */
+class Receiver extends Thread {
+    private BlockingQueue<DatagramPacket> queue;
+    private DatagramSocket               socket;
+
+    public Receiver(BlockingQueue<DatagramPacket> aQueue, 
+                    DatagramSocket aSocket) {
+        this.queue   = aQueue;
+        this.socket  = aSocket;
+    }
+
+    public void run() {
+        while(!this.socket.isClosed()) {
+            try {
+                byte         buf[] = new byte[8192];
+                DatagramPacket pkt = new DatagramPacket(buf, buf.length);
+                this.socket.receive(pkt);
+                this.queue.put(pkt);
+            } catch (InterruptedException e) {
+                /* We could not put it on the queue. This means the package
+                 * is dropped. However, we continue to listen */
+                continue;
+            } catch (IOException e) {
+                if (socket.isClosed())
+                    return;
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void close() {
+        /* close the socket */
+        this.socket.close();
+    }
+}
+
