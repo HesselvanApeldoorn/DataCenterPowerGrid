10149355bee5d9c935651d31ad2d0d34e2042e40
Bart Wiegmans <bartwiegmans@gmail.com>
2014-10-24 08:44:19 +0200

    Simplify ResendBuffer

--- DataCenterPowerGrid/src/ResendBuffer.java
+++ DataCenterPowerGrid/src/ResendBuffer.java
@@ -4,58 +4,38 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class ResendBuffer {
-    public Map<Long, List<Message>> sentMessages;
-    public Map<Long, List<Message>> deliveredMulticasts;
+    public Map<Long, List<Message>> messageLists;
 
     public ResendBuffer() {
-        sentMessages        = new HashMap<Long, List<Message>>(10);
-        deliveredMulticasts = new HashMap<Long, List<Message>>(10);
+        messageLists = new HashMap<Long, List<Message>>(10);
     }
 
-    public synchronized void addSentMessage(long receiver, Message message) {
+    public synchronized void add(long pid, Message message) {
         // messages are added in sequence order
-        if (!sentMessages.containsKey(receiver)) {
-            sentMessages.put(receiver, new ArrayList<Message>(10));
+        if (!messageLists.containsKey(pid)) {
+            messageLists.put(pid, new ArrayList<Message>(10));
         }
-        sentMessages.get(receiver).add(message);
+        messageLists.get(pid).add(message);
     }
 
-    public synchronized void addDeliveredMulticast(long sender, Message message) {
-        if (!deliveredMulticasts.containsKey(sender)) {
-            deliveredMulticasts.put(sender, new ArrayList<Message>(10));
-        }
-        deliveredMulticasts.get(sender).add(message);
-    }
 
-    public synchronized Message findSentMessage(long pid, int sequence_nr) {
-        return findInList(sentMessages.get(pid), sequence_nr);
-    }
-
-    public synchronized Message findDeliveredMulticast(long pid, int sequence_nr) {
-        return findInList(deliveredMulticasts.get(pid), sequence_nr);
-    }
-
-    private Message findInList(List<Message> list, int sequence_nr) {
+    public synchronized Message find(long pid, int sequence_nr) {
+        List<Message> list = messageLists.get(pid);
+        if (list == null || list.size() == 0)
+            return null;
         int  left = 0;
         int right = list.size();
-        int   mid = (left + right) / 2;
-        while (mid > left) {
+        while (left < right) {
+            int mid    = (left + right) / 2;
             int needle = list.get(mid).sequence_nr;
-            if (needle > sequence_nr) {
+            if (needle > sequence_nr)
+                left = mid + 1;
+            else if (needle < sequence_nr)
                 right = mid;
-                mid   = (left + right) / 2;
-            } else if (needle < sequence_nr) {
-                left = mid;
-                mid  = (left + right) / 2;
-            } else {
+            else
                 return list.get(mid);
-            }
         }
-        if (mid == right)
-            return null;
-        if (list.get(mid).sequence_nr == sequence_nr)
-            return list.get(mid);
-        return null;
+        return list.get(left).sequence_nr == sequence_nr ? list.get(left) : null;
     }
 
 }
